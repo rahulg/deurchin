696d946c3921b7fe08bd190c8360bdaa27f80774
Rahul AG <r@hul.ag>
2016-03-16 19:18:44 +0800

    switch to simple regexp-based replacement

--- deurchin.js
+++ deurchin.js
@@ -1,33 +1,19 @@
 "use strict";
 
-var urchins = /^utm_(?:content|campaign|source|medium)=/;
+var urchins = /([?&])utm_(?:content|campaign|source|medium)=[^&#]+/g;
 
 function stripUrchins(details) {
 
-	var hashSplit = details.url.split("#");
-	var uriFragment = hashSplit[1];
-
-	var paramSplit = hashSplit[0].split("?");
-	if (paramSplit.length < 2) {
-		return {};
+	function replaceSeparator(match, sep) {
+		return sep === "?" ? "?" : "";
 	}
 
-	var uriBase = paramSplit[0];
-	var params = paramSplit[1].split("&");
+	var newUrl = details.url.replace(urchins, replaceSeparator);
 
-	var filteredParams = params.filter(function(element) {
-		return !element.match(urchins);
-	});
-
-	if (filteredParams.length === params.length) {
-		return {};
+	if (newUrl !== details.url) {
+		return {redirectUrl: newUrl};
 	}
 
-	var newUri = uriBase + "?" + filteredParams.join("&");
-	if (uriFragment) {
-		newUri += "#" + uriFragment;
-	}
-
-	return {redirectUrl: newUri};
+	return {};
 
 }
